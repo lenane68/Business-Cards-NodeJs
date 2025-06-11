0d18e7810d6d8283cc84709098daf033357963b2
lena <lena.mhamed@hotmail.com>
2025-06-11 12:11:34 +0300

    c2

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use("/api/auth", auth);
 app.use("/api/cards", cards);
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/business_cards")
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ Connected to MongoDB..."))
   .catch((err) => console.error("❌ Could not connect to MongoDB...", err));
 
