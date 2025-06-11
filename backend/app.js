16f0e5c46c7cd5c9de172b1715378bb74a0dbbde
lena <lena.mhamed@hotmail.com>
2025-06-11 12:21:10 +0300

    b3

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,8 @@ const cards = require("./routes/cards");
 const app = express();
 app.use(express.json());
 
+app.use(cors());
+
 app.use("/api/users", users);
 app.use("/api/auth", auth);
 app.use("/api/cards", cards);
